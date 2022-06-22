4c738a25f4afa2cdd42a1002c08f2057d6ac0b2b
Henry Arasaki <henry_arasaki@hotmail.com>
2022-06-21 23:36:22 -0300

    home

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,13 @@ import theme from './styles/theme'
 
 import { Details } from './pages/Details'
 
+import { Home } from './pages/Home'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalStyles/>
-        <Details/>
+      <GlobalStyles />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>
 )
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,14 +1,45 @@
-import { Container } from "./styles"
+import { Container, Links, Content } from "./styles"
 
 import { Header } from "../../components/Header"
 import { Button } from "../../components/Button"
+import { Section } from "../../components/Section"
+import { Tag } from "../../components/Tag"
+import { ButtonText } from "../../components/ButtonText"
 
 export function Details() {
 
   return (
     <Container>
       <Header />
-        <Button title="voltar" />
+      <main>
+        <Content>
+
+          <ButtonText title="Excluir nota" />
+
+          <h1>Titulo</h1>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ipsum saepe cum, dolorum ut ex corrupti ea veritatis, laudantium tempora labore in dignissimos. Id voluptatum a amet facilis, necessitatibus recusandae.
+          </p>
+
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="https://www.youtube.com/">https://www.youtube.com/</a>
+              </li>
+
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express" />
+          </Section>
+
+          <Button title="voltar" />
+
+
+        </Content>
+      </main>
     </Container>
   )
 }
