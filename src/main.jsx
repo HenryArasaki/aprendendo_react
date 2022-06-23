6733040f99fef514a9fe187e79335feb9d2f1fda
Henry Arasaki <henry_arasaki@hotmail.com>
2022-06-23 00:23:18 -0300

    New

--- src/main.jsx
+++ src/main.jsx
@@ -9,11 +9,21 @@ import { Details } from './pages/Details'
 
 import { Home } from './pages/Home'
 
+import {SignIn} from './pages/SignIn'
+
+import {SignUp} from './pages/SignUp'
+
+import {Profile} from './pages/Profile'
+
+import {New} from './pages/New'
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <New />
     </ThemeProvider>
   </React.StrictMode>
 )
